5fd3145beda7b2b5f84b42f47851f9180ad0580f
= <=>
2017-11-18 21:53:35 +0545

    #representation-carousel and #job-carousel fixed and created, owl-carousel.default.min.css border-radius:0px and CSS organized

--- owlcarousel/init.js
+++ owlcarousel/init.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-$('.owl-carousel').owlCarousel({
+
+$('#representation-carousel').owlCarousel({
     loop:true,
     margin:10,
     autoplay: true,
@@ -16,6 +17,11 @@ $('.owl-carousel').owlCarousel({
             nav:true,
             dots:false
         },
+        568:{
+            items:3,
+            nav:true,
+            dots:false
+        },
         600:{
             items:3,
             nav:false,
@@ -28,9 +34,39 @@ $('.owl-carousel').owlCarousel({
             dots:true
         }
     }
-})
 });
 
-// $(document).ready(function(){
-//   $(".owl-carousel").owlCarousel();
-// });
+$('#job-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    responsiveClass:true,
+    nav:false,
+    responsive:{
+        0:{
+            items:1,
+            dots:false
+        },
+        320:{
+            items:1,
+            dots:false
+        },
+        568:{
+            items:1,
+            dots:false
+        },
+        600:{
+            items:1,
+            dots:true
+        },
+        1000:{
+            items:1,
+            loop:true,
+            dots:true
+        }
+    }
+});
+
+});
+
